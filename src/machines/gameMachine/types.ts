730e283146a95a94fe7184aafea6de29186bb9fa
Yongjin Huang <1765941460@qq.com>
2022-07-09 09:50:06 +0800

    Add Levels To Game Machine

--- src/machines/gameMachine/types.ts
+++ src/machines/gameMachine/types.ts
@@ -18,14 +18,26 @@ export interface HomeButtonClickedType {
     type: `HOME_BUTTON_CLICKED`;
 }
 
+export interface PlayerWalkedThroughDoor {
+    type: `PLAYER_WALKED_THROUGH_DOOR`;
+}
+
 export type GameEventType =
     | StartButtonClickedType
     | PlayerDiedType
     | PlayerGotTreasureType
     | RestartButtonClickedType
-    | HomeButtonClickedType;
+    | HomeButtonClickedType
+    | PlayerWalkedThroughDoor;
 
 export type GameStateType = {
     context: null;
-    value: `home` | `playing` | `gameOver` | `gameComplete`;
+    value:
+        | `home`
+        | `playing`
+        | `gameOver`
+        | `gameComplete`
+        | `playing.level1`
+        | `playing.level2`
+        | `playing.level3`;
 };
